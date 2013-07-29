e1757038fce25677a148769453a5c7e1a4a52ef3
Melvyn <melvyn.laily@gmail.com>
2013-07-29 11:58:18 -0400

    automatically reformated all the sources
    I'm not sure how I'm supposed to do that correctly...

--- Promises/Examples.cs
+++ Promises/Examples.cs
@@ -5,24 +5,24 @@ namespace Examples
 {
 	class MainClass
 	{
-        /// <summary>
-        /// Not a very useful example, just need a main class to compile and run the tests.  
-        /// </summary>
+		/// <summary>
+		/// Not a very useful example, just need a main class to compile and run the tests.  
+		/// </summary>
 		public static void Main(string[] args)
 		{
 			Promise<string> hello = CreateSimplePromise();
 			hello.success(mesg => Console.WriteLine(mesg));
 		}
 
-        /// <summary>
-        /// Create a simple synchronous promise.
-        /// </summary>
+		/// <summary>
+		/// Create a simple synchronous promise.
+		/// </summary>
 		public static Promise<string> CreateSimplePromise()
 		{
-            // The promise constructor takes a function that takes a callback. 
-            // By passing it's own contruction function as the required callback, 
-            // the promise can encapsulate the results returned via the callback.
-			Action<Action<PromiseError, string>> constructor = cb => 
+			// The promise constructor takes a function that takes a callback. 
+			// By passing it's own contruction function as the required callback, 
+			// the promise can encapsulate the results returned via the callback.
+			Action<Action<PromiseError, string>> constructor = cb =>
 			{
 				cb(null, "Hello World");
 			};
--- Tests/PromiseTests.cs
+++ Tests/PromiseTests.cs
@@ -3,136 +3,136 @@ using NUnit.Framework;
 
 namespace Promise
 {
-    [TestFixture]
-    public class PromiseTests
-    {
-        [Test]
-        public void TestBasicPromise()
-        {
-            Promise<int> p1 = new Promise<int>(cb => cb(null, 5));
-            bool test = false;
-            p1.success(s => test = true);
-            p1.success(s => Assert.AreEqual(s, 5));
-            Assert.IsTrue(test);
-        }
+	[TestFixture]
+	public class PromiseTests
+	{
+		[Test]
+		public void TestBasicPromise()
+		{
+			Promise<int> p1 = new Promise<int>(cb => cb(null, 5));
+			bool test = false;
+			p1.success(s => test = true);
+			p1.success(s => Assert.AreEqual(s, 5));
+			Assert.IsTrue(test);
+		}
 
-        [Test]
-        public void TestCombinePromise()
-        {
-            Promise<int> p1 = new Promise<int>(cb => cb(null, 5));
-            Promise<string> p2 = new Promise<string>(cb => cb(null, "some string"));
+		[Test]
+		public void TestCombinePromise()
+		{
+			Promise<int> p1 = new Promise<int>(cb => cb(null, 5));
+			Promise<string> p2 = new Promise<string>(cb => cb(null, "some string"));
 
-            var combo = p1.combine(p2);
+			var combo = p1.combine(p2);
 
-            bool test = false;
+			bool test = false;
 
-            combo.success(p =>
-            {
-                Assert.AreEqual(p.first, 5);
-                Assert.AreEqual(p.second, "some string");
-            });
+			combo.success(p =>
+			{
+				Assert.AreEqual(p.first, 5);
+				Assert.AreEqual(p.second, "some string");
+			});
 
-            combo.success(s => test = true);
+			combo.success(s => test = true);
 
-            Assert.IsTrue(test);
-        }
+			Assert.IsTrue(test);
+		}
 
-        [Test]
-        public void TestMapPromise()
-        {
-            Promise<int> p1 = new Promise<int>(cb => cb(null, 5));
-            Promise<string> p2 = p1.map(x => x.ToString());
+		[Test]
+		public void TestMapPromise()
+		{
+			Promise<int> p1 = new Promise<int>(cb => cb(null, 5));
+			Promise<string> p2 = p1.map(x => x.ToString());
 
-            p2.success(s => Assert.AreEqual(s, "5"));
+			p2.success(s => Assert.AreEqual(s, "5"));
 
-            bool test = false;
+			bool test = false;
 
-            p2.success(s => test = true);
+			p2.success(s => test = true);
 
-            Assert.IsTrue(test);
-        }
+			Assert.IsTrue(test);
+		}
 
-        [Test]
-        public void TestJoin()
-        {
-            Promise<Promise<string>> p = new Promise<Promise<string>>(cb => cb(null, new Promise<string>(cb2 => cb2(null, "gotcha"))));
+		[Test]
+		public void TestJoin()
+		{
+			Promise<Promise<string>> p = new Promise<Promise<string>>(cb => cb(null, new Promise<string>(cb2 => cb2(null, "gotcha"))));
 
-            p.success(s => Assert.AreNotEqual(s, "gotcha"));
-            p.success(s => s.success(y => Assert.AreEqual(y, "gotcha")));
+			p.success(s => Assert.AreNotEqual(s, "gotcha"));
+			p.success(s => s.success(y => Assert.AreEqual(y, "gotcha")));
 
-            var p2 = Promise.join(p);
+			var p2 = Promise.join(p);
 
-            p2.success(s => Assert.AreEqual(s, "gotcha"));
+			p2.success(s => Assert.AreEqual(s, "gotcha"));
 
-            bool test = false;
+			bool test = false;
 
-            p2.success(x => test = true);
+			p2.success(x => test = true);
 
-            Assert.IsTrue(test);
-        }
+			Assert.IsTrue(test);
+		}
 
-        [Test]
-        public void TestFlatMap()
-        {
-            Promise<int> p1 = new Promise<int>(cb => cb(null, 5));
-            Promise<string> p2 = p1.flatMap(i => new Promise<string>(cb => cb(null, i.ToString())));
+		[Test]
+		public void TestFlatMap()
+		{
+			Promise<int> p1 = new Promise<int>(cb => cb(null, 5));
+			Promise<string> p2 = p1.flatMap(i => new Promise<string>(cb => cb(null, i.ToString())));
 
-            p2.success(s => Assert.AreEqual(s, "5"));
+			p2.success(s => Assert.AreEqual(s, "5"));
 
-            bool test = false;
+			bool test = false;
 
-            p2.success(x => test = true);
+			p2.success(x => test = true);
 
-            Assert.IsTrue(test);
-        }
+			Assert.IsTrue(test);
+		}
 
-        [Test]
-        public void TestcompoundFunc()
-        {
-            Promise<string> suf = new Promise<string>(cb => cb(null, "second"));
+		[Test]
+		public void TestcompoundFunc()
+		{
+			Promise<string> suf = new Promise<string>(cb => cb(null, "second"));
 
-            Promise<string> full = suf.flatMap(x => convert("first", x));
+			Promise<string> full = suf.flatMap(x => convert("first", x));
 
-            full.success(x => Assert.AreEqual(x, "first, second"));
+			full.success(x => Assert.AreEqual(x, "first, second"));
 
-            bool test = false;
+			bool test = false;
 
-            full.success(x => test = true);
+			full.success(x => test = true);
 
-            Assert.IsTrue(test);
-        }
+			Assert.IsTrue(test);
+		}
 
-        public Promise<string> convert(string prefix, string suffix)
-        {
-            return new Promise<string>(cb => cb(null, prefix + ", " + suffix));
-        }
+		public Promise<string> convert(string prefix, string suffix)
+		{
+			return new Promise<string>(cb => cb(null, prefix + ", " + suffix));
+		}
 
-        [Test]
-        public void TestFailure()
-        {
-            Promise<long> p = new Promise<long>(cb => cb("uh oh", default(long)));
+		[Test]
+		public void TestFailure()
+		{
+			Promise<long> p = new Promise<long>(cb => cb("uh oh", default(long)));
 
-            bool test = false;
+			bool test = false;
 
-            p.success(x => test = true);
+			p.success(x => test = true);
 
-            Assert.IsFalse(test);
+			Assert.IsFalse(test);
 
-            p.fail(s => test = true);
+			p.fail(s => test = true);
 
-            Assert.IsTrue(test);
+			Assert.IsTrue(test);
 
-            var p2 = p.flatMap(l => new Promise<int>(cb => cb(null, 3)));
+			var p2 = p.flatMap(l => new Promise<int>(cb => cb(null, 3)));
 
-            test = false;
+			test = false;
 
-            p2.success(x => test = true);
+			p2.success(x => test = true);
 
-            Assert.IsFalse(test);
+			Assert.IsFalse(test);
 
-            p2.fail(s => test = true);
+			p2.fail(s => test = true);
 
-            Assert.IsTrue(test);
-        }
-    }
+			Assert.IsTrue(test);
+		}
+	}
 }
