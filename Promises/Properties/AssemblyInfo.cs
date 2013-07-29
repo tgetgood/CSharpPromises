ce694867e729fd3bf1fdf0e18d5379057b759ba7
Melvyn <melvyn.laily@gmail.com>
2013-07-29 12:13:51 -0400

    readded an AssemblyInfo file to fix the compiler warning

---
+++ Promises/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Promises")]
+[assembly: AssemblyDescription("Promises")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Taste Filter Inc.")]
+[assembly: AssemblyProduct("Promises.Properties")]
+[assembly: AssemblyCopyright("Copyright Taste Filter Inc. (2013). All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a58bb577-4a39-41fa-aae0-28b2941a7889")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
