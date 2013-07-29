45335fe6a776347676cf63b08384ea39a8706608
Melvyn <melvyn.laily@gmail.com>
2013-07-29 19:25:33 -0400

    moved the examples to their own project. the Promises project now compiles as a dll
    also added an AssemblyInfo to the test project I had forgotten.

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,10 +1,15 @@
+﻿using Promises;
 using System;
-using Promises;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Examples
 {
-	class MainClass
+	class Program
 	{
+
 		/// <summary>
 		/// Not a very useful example, just need a main class to compile and run the tests.  
 		/// </summary>
@@ -12,6 +17,7 @@ namespace Examples
 		{
 			Promise<string> hello = CreateSimplePromise();
 			hello.Success(mesg => Console.WriteLine(mesg));
+			Console.ReadLine();
 		}
 
 		/// <summary>
@@ -22,7 +28,7 @@ namespace Examples
 			// The promise constructor takes a function that takes a callback. 
 			// By passing it's own contruction function as the required callback, 
 			// the promise can encapsulate the results returned via the callback.
-			Action<Action<PromiseError, string>> constructor = cb =>
+			Action<Action<PromiseError, string>> constructor = (cb) =>
 			{
 				cb(null, "Hello World");
 			};
--- Examples/Properties/AssemblyInfo.cs
+++ Examples/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Promises")]
-[assembly: AssemblyDescription("Promises")]
+[assembly: AssemblyTitle("Examples")]
+[assembly: AssemblyDescription("Examples")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Taste Filter Inc.")]
-[assembly: AssemblyProduct("Promises")]
+[assembly: AssemblyProduct("Examples")]
 [assembly: AssemblyCopyright("Copyright Taste Filter Inc. (2013). All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a58bb577-4a39-41fa-aae0-28b2941a7889")]
+[assembly: Guid("0cf8dd4f-cf23-46b6-8e54-a70d4711cbf2")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,5 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
